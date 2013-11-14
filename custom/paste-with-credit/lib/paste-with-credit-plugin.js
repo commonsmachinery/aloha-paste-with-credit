aeface43006679fab1a12b8a3a16501c1670efc2
Peter Liljenberg <peter.liljenberg@gmail.com>
2013-11-14 20:50:03 +0100

    Use libcredit to generate the attribution

--- custom/paste-with-credit/lib/paste-with-credit-plugin.js
+++ custom/paste-with-credit/lib/paste-with-credit-plugin.js
@@ -15,7 +15,7 @@ define([
 	'block/block',
 	'block/blockmanager',
 	'aloha/copypaste',
-	'paste-with-credit/vendor/rdflib',
+	'libcredit',
 	'css!paste-with-credit/css/paste-with-credit.css'
 ], function (
 	jQuery,
@@ -24,43 +24,13 @@ define([
 	Commands,
 	Block,
 	BlockManager,
-	CopyPaste
+	CopyPaste,
+        libcredit
 ) {
 	'use strict';
 
 	var idNum = 0; // counter for having unique IDs for #abouts
 
-	var CommonLicenses = {
-		"http://creativecommons.org/licenses/by/3.0/": "Creative Commons Attribution 3.0 Unported License",
-		"http://creativecommons.org/licenses/by-nc/3.0/": "Creative Commons Attribution-NonCommercial 3.0 Unported License",
-		"http://creativecommons.org/licenses/by-nc-nd/3.0/": "Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License",
-		"http://creativecommons.org/licenses/by-nc-sa/3.0/": "Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported License",
-		"http://creativecommons.org/licenses/by-nd/3.0/": "Creative Commons Attribution-NoDerivs 3.0 Unported License",
-		"http://creativecommons.org/licenses/by-sa/3.0/": "Creative Commons Attribution-ShareAlike 3.0 Unported License",
-	};
-
-	// parseRDFXML, createSpan, createA and createAttribution
-	// adapted from Peter Liljenberg's paste.js example,
-	// http://brugd.ctrl-c.liu.se/~petli/examples/paste/paste.html
-
-	var DC = $rdf.Namespace('http://purl.org/dc/elements/1.1/');
-	var DCTERMS = $rdf.Namespace('http://purl.org/dc/terms/');
-	var CC = $rdf.Namespace('http://creativecommons.org/ns#');
-	var XHTML = $rdf.Namespace('http://www.w3.org/1999/xhtml/vocab#');
-	var RDF = $rdf.Namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#"');
-	var OG = $rdf.Namespace('http://ogp.me/ns#');
-
-	// Parse the RDF/XML using https://github.com/linkeddata/rdflib.js
-	function parseRDFXML(str) {
-		var dom = new DOMParser().parseFromString(str, 'application/xml');
-
-		var kb = new $rdf.IndexedFormula();
-		var parser = new $rdf.RDFParser(kb);
-
-		parser.parse(dom, '', null);
-
-		return kb;
-	};
 
 	function createSpan(content, property) {
 		var span = jQuery('<span/>');
@@ -90,129 +60,6 @@ define([
 
 		return a;
 	}
-
-	// Get CC-related properties about the work
-	function createAttribution(kb, srcURI, targetURI) {
-		var creditDiv = jQuery('<div/>', {
-			about: targetURI,
-		});
-		
-		var root = kb.sym(srcURI);
-
-		// Could go look for different titles in an rdf:Alt node and
-		// choose one based on language, and also look for dcterms:title.
-		var title = kb.any(root, DC('title')) ||
-			kb.any(root, OG('title'));
-
-		// Use canonical URL for link, if any
-		var url = kb.any(root, OG('url'));
-
-		if (url) {
-			url = url.uri || url.value;
-		}
-		else {
-			// Fall back on subject URI, which is better than nothing
-			url = srcURI;
-		}
-
-		var creator = kb.any(root, DC('creator'));
-
-		var attributionURL = kb.any(root, CC('attributionURL'));
-		if (attributionURL != null)
-			attributionURL = attributionURL.uri;
-
-		var attributionName = kb.any(root, CC('attributionName'));
-		if (!attributionName)
-			if (creator)
-				attributionName = creator;
-			else
-				attributionName = attributionURL;
-
-		// Use the first of xhtml:license, dcterms:license and cc:license
-		var license = kb.any(root, XHTML('license'));
-		if (license == null)
-			license = kb.any(root, DCTERMS('license'));
-		if (license == null)
-			license = kb.any(root, CC('license'));
-		if (license != null)
-			license = license.uri;
-
-		// build attribution
-		if (title) {
-			if (url) {
-				creditDiv.append(createA(url, title,
-					'http://purl.org/dc/elements/1.1/source',
-					'http://purl.org/dc/elements/1.1/title'));
-			} else {
-				creditDiv.append(createSpan(title, 'http://purl.org/dc/elements/1.1/title'));
-			}
-		} else {
-			creditDiv.append('Image');
-		}
-
-		if (attributionName) {
-			creditDiv.append(' by ');
-
-			if (attributionURL) {
-				creditDiv.append(createA(attributionURL, attributionName,
-					'http://creativecommons.org/ns#attributionURL',
-					'http://creativecommons.org/ns#attributionName'));
-			} else {
-				 creditDiv.append(createSpan(attributionName,
-				   'http://creativecommons.org/ns#attributionName'));
-			}
-		} else {
-			// has no attribution
-		}
-
-		creditDiv.append('.');
-
-		if (license) {
-			var licenseLabel = license;
-			if (CommonLicenses[license])
-				licenseLabel = CommonLicenses[license];
-				
-			creditDiv.append(' Licensed under ');
-			creditDiv.append(createA(license, licenseLabel, 'license', null));
-			creditDiv.append('.');
-		}
-
-		// sources
-		var sources = kb.each(root, DC('source'));
-		if (sources.length > 0) {
-			creditDiv.append(' Source works:');
-
-			var sourceList = jQuery("<ul/>");
-			creditDiv.append(sourceList);
-
-			for (var i in sources) {
-				var sourceUri = sources[i].uri;
-				var sourceTitle = kb.any(sources[i], DC('title'));
-				var sourceAuthor = kb.any(sources[i], CC('attributionName'));
-				if (sourceAuthor == null)
-					sourceAuthor = kb.any(sources[i], DC('creator'));
-				var li = jQuery("<li/>");
-				li.attr("about", sourceUri);
-
-				if (sourceTitle == null)
-					sourceTitle = sourceUri;
-
-				li.append(createA(sourceUri, sourceTitle, 'http://purl.org/dc/elements/1.1/source', null));
-
-				if (sourceAuthor) {
-					li.append(" by ");
-					li.append(createSpan(sourceAuthor, 'http://creativecommons.org/ns#attributionName'));
-				}
-
-				sourceList.append(li);
-			}
-
-			//creditDiv.append('.');
-		}
-		
-		return creditDiv;
-	};
-
 
 	var ImageWithCreditBlock = Block.AbstractBlock.extend({
 		title: 'Image with credit'
@@ -258,22 +105,23 @@ define([
 					blockDiv.append(imgDiv);
 
 					if (detail.rdfxml) {
-						var kb = parseRDFXML(detail.rdfxml);
+                                            var doc, credit, formatter, captionDiv;
 
-						// Find the source image
-						var source = kb.any(kb.sym(''), DC('source'));
+                                            doc = new DOMParser().parseFromString(detail.rdfxml, 'text/xml');
+                                            credit = libcredit.credit(libcredit.parseRDFXML(doc));
 
-						if (source) {
-							var credit = createAttribution(kb, source.uri, '#' + imgDiv.attr('id'));
-							var captionDiv = jQuery('<div/>');
+                                            formatter = libcredit.htmlCreditFormatter(document);
+                                            credit.format(formatter, 2);
 
-							// do we need to leave captions editable?
-							captionDiv.addClass('aloha-editable');
-							captionDiv.addClass('image-with-credit-caption');
+					    captionDiv = jQuery('<div/>');
+                                            
+					    // do we need to leave captions editable?
+					    captionDiv.addClass('aloha-editable');
+					    captionDiv.addClass('image-with-credit-caption');
+                                            
+                                            captionDiv.append(formatter.getRoot());
 
-							captionDiv.append(credit);
-							blockDiv.append(captionDiv);
-						}
+					    blockDiv.append(captionDiv);
 					}
 
 					GENTICS.Utils.Dom.insertIntoDOM(blockDiv, range, jQuery(Aloha.activeEditable.obj), true);
